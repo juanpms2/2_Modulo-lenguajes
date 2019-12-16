69eda8d02faecb0e2fb64cff24e2f505bbb4b5e2
Juanpi <jpabloms2@hotmail.com>
2019-12-16 19:17:19 +0100

    Función tail ok

--- src/main.js
+++ src/main.js
@@ -1,11 +1,20 @@
 // Implementa una función head (inmutable), tal que, dado un array como entrada extraiga y devuelva su primer elemento. Utiliza destructuring.
 
-const myArray = [1,2,3,4,5];
+const myArray = [ 1, 2, 3, 4, 5 ];
  
-const head = (array) => { 
-    const [first, ...rest] = array; 
+const head = ( array ) => { 
+    const [ first, ] = array; 
     return first; 
 }
-console.log(head(myArray));
+console.log( head( myArray ) );
 
 
+// Implementa una función tail (inmutable), tal que, dado un array como entrada devuelta todos menos el primer elemento. Utiliza rest operator.
+
+const tail = ( array ) => {
+    const [ , ...rest ] = array;
+    rest.forEach(element => {
+        console.log( element );
+    });
+}
+tail( myArray );
