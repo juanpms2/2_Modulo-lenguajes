e69bf3aaa64ed7e4d97c94db39bc9d2726ca36d6
Juanpi <jpabloms2@hotmail.com>
2019-12-17 09:02:11 +0100

    Función init ok y funciones tail y head corregidas

--- src/main.js
+++ src/main.js
@@ -1,9 +1,10 @@
-// Implementa una función head (inmutable), tal que, dado un array como entrada extraiga y devuelva su primer elemento. Utiliza destructuring.
 
 const myArray = [ 1, 2, 3, 4, 5 ];
+
+// Implementa una función head (inmutable), tal que, dado un array como entrada extraiga y devuelva su primer elemento. Utiliza destructuring.
  
-const head = ( array ) => { 
-    const [ first, ] = array; 
+const head = ( [...array] ) => { 
+    const [first, ] = array;
     return first; 
 }
 console.log( head( myArray ) );
@@ -11,10 +12,19 @@ console.log( head( myArray ) );
 
 // Implementa una función tail (inmutable), tal que, dado un array como entrada devuelta todos menos el primer elemento. Utiliza rest operator.
 
-const tail = ( array ) => {
+const tail = ( [...array] ) => {
     const [ , ...rest ] = array;
     rest.forEach(element => {
         console.log( element );
     });
 }
 tail( myArray );
+
+
+// Implementa una función init (inmutable), tal que, dado un array como entrada devuelva todos los elementos menos el último. Utiliza los métodos que ofrece Array.prototype.
+
+const init = ( [...array] ) => {
+    const x = array.length -1;
+    console.log(array.slice(x).toString());
+}
+init(myArray);
